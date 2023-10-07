09dbe3a5e157d95157b08af6a71f0fea9a3039b7
Amir55143 <aamirnoor55143@gmail.com>
2023-10-07 21:07:31 +0500

    Json/Object

--- lib/model/note_model.dart
+++ lib/model/note_model.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class NoteModel {
   final String? id;
   final String? title;
@@ -5,4 +7,21 @@ class NoteModel {
   final int? color;
 
   NoteModel({this.id, this.title, this.body, this.color});
+
+  factory NoteModel.fromSnapshot(
+      DocumentSnapshot<Map<String, dynamic>> snapshot) {
+    return NoteModel(
+      id: snapshot['id'],
+      title: snapshot['title'],
+      body: snapshot['body'],
+      color: snapshot['color'],
+    );
+  }
+
+  Map<String, dynamic> toDocument() => {
+        "id": id,
+        "title": title,
+        "body": body,
+        "color": color,
+      };
 }
