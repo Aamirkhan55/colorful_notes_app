29cf7a78af928b8f51af91623c81164b4a9a7b55
Amir55143 <aamirnoor55143@gmail.com>
2023-10-07 22:03:46 +0500

    Read/Get

--- lib/database/db_handler.dart
+++ lib/database/db_handler.dart
@@ -1,9 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:colorful_notes_app/model/note_model.dart';
 
-
 class DatabaseHandler {
-  static Future<void> createNote (NoteModel note) async{
+  // Create Note
+  static Future<void> createNote(NoteModel note) async {
     final noteCollection = FirebaseFirestore.instance.collection('notes');
     final id = noteCollection.doc().id;
     final newNote = NoteModel(
@@ -16,7 +16,15 @@ class DatabaseHandler {
     try {
       noteCollection.doc(id).set(newNote);
     } catch (e) {
-     "Some Error Occure $e";
+      "Some Error Occure $e";
     }
   }
+
+  // Read Note
+  static Stream<List<NoteModel>> getNotes() {
+    final noteCollection = FirebaseFirestore.instance.collection('notes');
+    return noteCollection.snapshots().map((querySnapshot) =>
+        querySnapshot.docs.map((e) => NoteModel.fromSnapshot(e)).toList());
+  }
+
 }
